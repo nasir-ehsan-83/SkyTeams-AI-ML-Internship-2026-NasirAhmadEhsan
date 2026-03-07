b42be3b6e12fcee4fbb045ee5c4688b22e9982a7
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-03-07 06:27:50 +0430

    feat: add input validation functions to prevent negative input

--- src/python_fundamental/loop_operations/factorial.py
+++ src/python_fundamental/loop_operations/factorial.py
@@ -1,14 +1,32 @@
+from typing import Optional
+
+# add this function for input validation(is inputer greater or equal to 0)
+def fact_loop(number: int) -> Optional[int]:
+    # if number positive or 0
+    if number >= 0:
+        __fact_loop(number)
+    else:
+        return None
+
 # compute factorial by loop
-def fact_loop(number: int) -> int:
+def __fact_loop(number: int) -> int:
     factorial = 1
     for i in range(1, number + 1):
         factorial *= i
 
     return factorial
 
+# add this function for input validation(is input greater or equal to 0)
+def fact_recursion(number: int) -> Optional[int]:
+    # if number positive or 0
+    if number >= 0:
+        __fact_recursion(number)
+    else:
+        return None
+
 # compute factorial by recursion
-def fact_recursion(number: int) -> int:
+def __fact_recursion(number: int) -> int:
     if number == 0:
         return 1
-    return number * fact_recursion(number - 1)
+    return number * __fact_recursion(number - 1)
     
