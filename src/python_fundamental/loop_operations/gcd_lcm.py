ecdc3113ab294b0bcb5cb16968b203984ec824f6
nasir-ehsan <nasirahmadehsan1383@gmail.com>
2026-03-08 21:07:50 +0430

    refactor: add input validation for gcd() and lcm()

--- src/python_fundamental/loop_operations/gcd_lcm.py
+++ src/python_fundamental/loop_operations/gcd_lcm.py
@@ -1,9 +1,17 @@
-# compute GCD of two numbers
-def gcd(num1: int, num2: int) -> int :
-	while num2 != 0:
-		num1, num2 = num2, num1 % num2
-	return num1
+from typing import Optional
 
+# compute GCD of two numbers
+def gcd(num1: int, num2: int) -> Optional[int] :
+	if num1 != 0 or num2 != 0:
+		while num2 != 0:
+			num1, num2 = num2, num1 % num2
+		return num1
+	else:
+		return None
+	
 # compute LCM of two numbers
-def lcm(num1: int, num2: int) -> int:
-	return (num1 * num2) // gcd(num1, num2)
+def lcm(num1: int, num2: int) -> Optional[int]:
+	if num1 != 0 or num2 != 0:
+		return (num1 * num2) // gcd(num1, num2)
+	else:
+		return None
