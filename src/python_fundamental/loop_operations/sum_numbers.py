c60314ebed105b40edaa22be2a8d6fed1dc2d87a
nasir-ehsan <nasirahmadehsan1383@gmail.com>
2026-03-08 22:03:48 +0430

    refactor: add input validation to prevent negative values and add Optional typing

--- src/python_fundamental/loop_operations/sum_numbers.py
+++ src/python_fundamental/loop_operations/sum_numbers.py
@@ -1,27 +1,38 @@
-# some of all natural numbers from 1 to n
-def sum_of_natural_number(n: int) -> int:
-    sum = 0
-    for i in range(n):
-        sum += i
+from typing import Optional
 
-    return sum
+# some of all natural numbers from 1 to n
+def sum_of_natural_number(number: int) -> Optional[int]:
+    if number > 0:
+        sum = 0
+        for i in range(number):
+            sum += i
+
+        return sum
+
+    return None
 
 # sum of all odd numbers from 0 to n
-def sum_of_odd_number(n: int) -> int:
-    sum_of_odd = 0
+def sum_of_odd_number(number: int) -> Optional[int]:
+    if number >= 0:
+        sum_of_odd = 0
 
-    for i in range(n):
-        if i % 2 != 0:
-            sum_of_odd += i
+        for i in range(number):
+            if i % 2 != 0:
+                sum_of_odd += i
+        
+        return sum_of_odd
     
-    return sum_of_odd
+    return None
 
 # sum of all even numbers from 0 to n
-def sum_of_even_number(n: int) -> int:
-    sum_of_even = 0
+def sum_of_even_number(number: int) -> Optional[int]:
+    if number >= 0:
+        sum_of_even = 0
 
-    for i in range(n):
-        if i % 2 == 0:
-            sum_of_even += i
+        for i in range(number):
+            if i % 2 == 0:
+                sum_of_even += i
 
-    return sum_of_even
+        return sum_of_even
+    
+    return None
