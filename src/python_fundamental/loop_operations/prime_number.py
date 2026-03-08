7474eacb76a710c4647bad9a1c2b8f5d01f51ada
nasir-ehsan <nasirahmadehsan1383@gmail.com>
2026-03-08 21:33:38 +0430

    feat: add validation input and Optional typing

--- src/python_fundamental/loop_operations/prime_number.py
+++ src/python_fundamental/loop_operations/prime_number.py
@@ -1,3 +1,8 @@
-def is_prtime(number: int) -> bool:
+from typing import Optional
+
+def is_prime(number: int) -> Optional[bool]:
     # if the number is not divisible by 2, 3, 5 is a prime number
-    return number % 2 != 0 and number % 3 != 0 and number % 5 != 0
+    if number != 0:
+        return number % 2 != 0 and number % 3 != 0 and number % 5 != 0
+    else:
+        return None
