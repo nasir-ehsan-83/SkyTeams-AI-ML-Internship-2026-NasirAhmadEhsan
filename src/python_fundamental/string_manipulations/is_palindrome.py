8222c4b31515e44b19f86d7083b763c9c3a68c73
nasir-ehsan <nasirahmadehsan1383@gmail.com>
2026-03-09 06:53:13 +0430

    refactor: add check pattern for sentences

--- src/python_fundamental/string_manipulations/is_palindrome.py
+++ src/python_fundamental/string_manipulations/is_palindrome.py
@@ -1,5 +1,24 @@
+from typing import List
+
 from reverse_string import reverse
 
 def is_palindrome(text: str) -> bool:
-    # if text and it's reversed case are equal means that the text is palindrome
+    if text.__contains__(" "):
+        word_list: List[int] = text.split(" ")
+    
+        i: int = 0
+        j: int = len(word_list) - 1
+        n: int = len(word_list) // 2
+
+        while i <= n <= j:
+            key: str = word_list[i]
+            word_list[i] = word_list[j]
+            word_list[j] = key
+
+            i += 1
+            j -= 1
+        
+        return text.split(" ") == word_list
+    
+    # if text is a word and it's reversed case are equal means that the text is palindrome
     return text == reverse(text)
